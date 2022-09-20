f2405030ac30ad80a0904f4fb20f169a35f457a5
YismaelDiazH <20213tn007@utez.edu.mx>
2022-09-20 06:23:21 -0500

    .

--- CalculadoraRpc/src/main/java/client/ClientRPC.java
+++ CalculadoraRpc/src/main/java/client/ClientRPC.java
@@ -15,7 +15,9 @@ public class ClientRPC {
         config.setServerURL(new URL("http://localhost:1200"));
         XmlRpcClient client = new XmlRpcClient();
         client.setConfig(config);
-        String option = "", firsNumber = "", secondNumber = "";
+        DaoCalculadora dao = new DaoCalculadora();
+        BeanCalculadora bean = new BeanCalculadora();
+        String option = "", firstNumber = "", secondNumber = "";
         do {
             System.out.println("1. Suma");
             System.out.println("2. Resta");
@@ -30,123 +32,169 @@ public class ClientRPC {
                 switch (Integer.parseInt(option)){
                     case 1:
                         System.out.println("SUMA");
+                        bean.setType("SUMA");
                         do{
                             System.out.println("Ingrese el Primer numero");
-                            firsNumber = sc.next();
-                            if (!isDouble(firsNumber))
+                            firstNumber = sc.next();
+                            bean.setFirst_number(Double.parseDouble(firstNumber));
+                            if (!isDouble(firstNumber))
                                 System.err.println("Ingrese un número válido. ");
-                        }while (!isDouble(firsNumber));
+                        }while (!isDouble(firstNumber));
                         do{
                             System.out.println("Ingrese el segundo numero");
                             secondNumber = sc.next();
+                            bean.setSecond_number(Double.parseDouble(secondNumber));
                             if (!isDouble(secondNumber))
                                 System.err.println("Ingrese un número válido. ");
                         }while (!isDouble(secondNumber));
                         //Ejecución del método en el servidor...
-                        Object[] data = {firsNumber, secondNumber};
+                        Object[] data = {firstNumber, secondNumber};
                         double response = (double) client.execute("Methods.addition", data);
                         System.out.println("Result -> " + response);
+                        bean.setResult(response);
+                        dao.saveAOperacion(bean);
                         break;
                     case 2:
                     System.out.println("RESTA");
-                    do{
+                        bean.setType("RESTA");
+
+                        do{
                         System.out.println("Ingrese el Primer numero");
-                        firsNumber = sc.next();
-                        if (!isDouble(firsNumber))
+                        firstNumber = sc.next();
+                        bean.setFirst_number(Double.parseDouble(firstNumber));
+
+                        if (!isDouble(firstNumber))
                             System.err.println("Ingrese un número válido. ");
-                    }while (!isDouble(firsNumber));
+                    }while (!isDouble(firstNumber));
                     do{
                         System.out.println("Ingrese el segundo numero");
                         secondNumber = sc.next();
+                        bean.setSecond_number(Double.parseDouble(secondNumber));
                         if (!isDouble(secondNumber))
                             System.err.println("Ingrese un número válido. ");
                     }while (!isDouble(secondNumber));
                     //Ejecución del método en el servidor...
-                    Object[] data2 = {firsNumber, secondNumber};
+                    Object[] data2 = {firstNumber, secondNumber};
                     double response2 = (double) client.execute("Methods.subtraction", data2);
                     System.out.println("Result -> " + response2);
+                        bean.setResult(response2);
+                        dao.saveAOperacion(bean);
                     break;
                     case 3:
                         System.out.println("Multiplicacion");
+                        bean.setType("MULTIPLICACION");
+
                         do{
                             System.out.println("Ingrese el Primer numero");
-                            firsNumber = sc.next();
-                            if (!isDouble(firsNumber))
+                            firstNumber = sc.next();
+                            bean.setFirst_number(Double.parseDouble(firstNumber));
+
+                            if (!isDouble(firstNumber))
                                 System.err.println("Ingrese un número válido. ");
-                        }while (!isDouble(firsNumber));
+                        }while (!isDouble(firstNumber));
                         do{
                             System.out.println("Ingrese el segundo numero");
                             secondNumber = sc.next();
+                            bean.setSecond_number(Double.parseDouble(secondNumber));
                             if (!isDouble(secondNumber))
                                 System.err.println("Ingrese un número válido. ");
                         }while (!isDouble(secondNumber));
                         //Ejecución del método en el servidor...
-                        Object[] data3 = {firsNumber, secondNumber};
+                        Object[] data3 = {firstNumber, secondNumber};
                         double response3 = (double) client.execute("Methods.multiplication", data3);
                         System.out.println("Result -> " + response3);
+                        bean.setResult(response3);
+                        dao.saveAOperacion(bean);
                         break;
                     case 4:
                         System.out.println("Division");
+                        bean.setType("DIVISION");
+
                         do{
                             System.out.println("Ingrese el Primer numero");
-                            firsNumber = sc.next();
-                            if (!isDouble(firsNumber))
+                            firstNumber = sc.next();
+                            bean.setFirst_number(Double.parseDouble(firstNumber));
+
+                            if (!isDouble(firstNumber))
                                 System.err.println("Ingrese un número válido. ");
-                        }while (!isDouble(firsNumber));
+                        }while (!isDouble(firstNumber));
                         do{
                             System.out.println("Ingrese el segundo numero");
                             secondNumber = sc.next();
+                            bean.setSecond_number(Double.parseDouble(secondNumber));
                             if (!isDouble(secondNumber))
                                 System.err.println("Ingrese un número válido. ");
                         }while (!isDouble(secondNumber));
                         //Ejecución del método en el servidor...
-                        Object[] data4 = {firsNumber, secondNumber};
+                        Object[] data4 = {firstNumber, secondNumber};
                         double response4 = (double) client.execute("Methods.division", data4);
                         System.out.println("Result -> " + response4);
+                        bean.setResult(response4);
+                        dao.saveAOperacion(bean);
                         break;
                     case 5:
                         System.out.println("Exponente");
+                        bean.setType("EXPONENTE");
+
                         do{
                             System.out.println("Ingrese la base");
-                            firsNumber = sc.next();
-                            if (!isDouble(firsNumber))
+                            firstNumber = sc.next();
+                            bean.setFirst_number(Double.parseDouble(firstNumber));
+
+                            if (!isDouble(firstNumber))
                                 System.err.println("Ingrese un número válido. ");
-                        }while (!isDouble(firsNumber));
+                        }while (!isDouble(firstNumber));
                         do{
                             System.out.println("Ingrese el exponente");
                             secondNumber = sc.next();
-
+                            bean.setSecond_number(Double.parseDouble(secondNumber));
                             if (!isDouble(secondNumber))
                                 System.err.println("Ingrese un número válido. ");
                         }while (!isDouble(secondNumber));
                         //Ejecución del método en el servidor...
-                        Object[] data5 = {firsNumber, secondNumber};
+                        Object[] data5 = {firstNumber, secondNumber};
                         double response5 = (double) client.execute("Methods.exponent", data5);
                         System.out.println("Result -> " + response5);
+                        bean.setResult(response5);
+                        dao.saveAOperacion(bean);
                         break;
                     case 6:
                         System.out.println("Raiz");
+                        bean.setType("RAIZ");
+
                         do{
                             System.out.println("Ingrese el numero");
-                            firsNumber = sc.next();
-                            if (!isDouble(firsNumber))
+                            firstNumber = sc.next();
+                            bean.setFirst_number(Double.parseDouble(firstNumber));
+
+                            if (!isDouble(firstNumber))
                                 System.err.println("Ingrese un número válido. ");
-                        }while (!isDouble(firsNumber));
+                        }while (!isDouble(firstNumber));
                         do{
                             System.out.println("Ingrese la raiz");
                             secondNumber = sc.next();
-
+                            bean.setSecond_number(Double.parseDouble(secondNumber));
                             if (!isDouble(secondNumber))
                                 System.err.println("Ingrese un número válido. ");
                         }while (!isDouble(secondNumber));
                         //Ejecución del método en el servidor...
-                        Object[] data6 = {firsNumber, secondNumber};
+                        Object[] data6 = {firstNumber, secondNumber};
                         double response6 = (double) client.execute("Methods.root", data6);
                         System.out.println("Result -> " + response6);
+                        bean.setResult(response6);
+                        dao.saveAOperacion(bean);
                         break;
+                    case 7:
+                        System.out.println("CONSULTA DE HISTORIAL");
+                        System.out.println(bean.toString());
 
+                        break;
+                    case 8:
+                        System.out.println("saliendo...");
+                        break;
                     default:
-                        System.out.println("No existe esa opción");
+                        System.out.println("Eso no existe...");
+                        break;
                 }
             }else {
                 System.out.println("La opción es incorrecta. Intente nuevamente");
